62d29529f66ebb6a2c1dd76e2f4164d78f8a15ca
rajatprajapati0 <rajatprajapati09@gmail.com>
2023-01-30 02:33:37 +0530

    Added: UC2 Program file

--- Program.cs
+++ Program.cs
@@ -9,25 +9,51 @@ namespace Line_Comparison
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Welcome to Line Comparison Computation Program ");
-           
+            Console.WriteLine("Welcome to Line Comparison Computation Program \n ");
+            lineUse();
             
 
-        
-           double one =Program.line();
-           
         }
-
-        static double line()
+        public static void lineUse()
         {
-            Console.WriteLine("give some input for a line");
-            Console.WriteLine("Enter first two numbers for x1 and y1");
+            double one = 0;
+            double two = 0;
+            for (int i=1;i<=2;i++) 
+            {
+                if (i==1)
+                {
+                    Console.WriteLine("enter valuse for first line \n");
+                     one = line();
+                    Console.WriteLine("\nlength of line one -"+one+"\n");                      
+                }
+            
+                else
+                {
+                    Console.WriteLine("enter valuse for second line");
+                     two= line();
+                    Console.WriteLine("\nlength of line two -" + two+"\n");
+
+                }
+            }
+            if (one==two) 
+            {
+                Console.WriteLine("lines are equal it is preferable.");
+            }
+            else
+            { Console.WriteLine("lines are not equal it is not preferable.");
+            }
+        }
+       static double line()
+        {
+
+            Console.WriteLine("give some input for a line\n");
+            Console.WriteLine("Enter first two numbers for x1 and y1\n");
             Console.WriteLine("Enter x1");
             double x1 = double.Parse(Console.ReadLine());
             Console.WriteLine("Enter y1");
             double y1 = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter first two numbers for x2 and y2");
+            Console.WriteLine("Enter first two numbers for x2 and y2 \n");
             Console.WriteLine("Enter x2");
             double x2 = double.Parse(Console.ReadLine());
             Console.WriteLine("Enter y2");
@@ -36,12 +62,10 @@ namespace Line_Comparison
 
             double Length_of_Line = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 
-            Console.WriteLine(Length_of_Line);
 
             return Length_of_Line;
         }
       
 
-        
     }
 }
