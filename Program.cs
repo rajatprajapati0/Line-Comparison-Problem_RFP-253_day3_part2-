652f4c9ce860b43f35dc41c01b50b5599ab58cbb
rajatprajapati0 <rajatprajapati09@gmail.com>
2023-01-30 01:44:33 +0530

    Added: UC1 Program file

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Transactions;
 
 namespace Line_Comparison
 {
@@ -6,11 +8,40 @@ namespace Line_Comparison
     {
         static void Main(string[] args)
         {
-       
+
             Console.WriteLine("Welcome to Line Comparison Computation Program ");
+           
+            
+
         
-        
-        
+           double one =Program.line();
+           
         }
+
+        static double line()
+        {
+            Console.WriteLine("give some input for a line");
+            Console.WriteLine("Enter first two numbers for x1 and y1");
+            Console.WriteLine("Enter x1");
+            double x1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter y1");
+            double y1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter first two numbers for x2 and y2");
+            Console.WriteLine("Enter x2");
+            double x2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter y2");
+            double y2 = double.Parse(Console.ReadLine());
+
+
+            double Length_of_Line = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+            Console.WriteLine(Length_of_Line);
+
+            return Length_of_Line;
+        }
+      
+
+        
     }
 }
